1437ffb61112df9f511c1240778890dd6b2316ac
Yogesh Jain <yogeshjain808@gmail.com>
2016-04-25 21:48:08 +0530

    Fixed color issue
    Added bitmap maker and fix coords
    
    
    cheers.

--- TEST/app/src/main/java/dmm8396/rit/edu/test/Test.java
+++ TEST/app/src/main/java/dmm8396/rit/edu/test/Test.java
@@ -2,6 +2,7 @@ package dmm8396.rit.edu.test;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -30,41 +31,50 @@ public class Test extends Activity {
         bpv.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                float x = v.getX();
-                float y = v.getY();
-                Bitmap bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.RGB_565);
+                float x = event.getX();
+                float y = event.getY();
+
+                Bitmap bitmap = loadBitmapFromView(v);
                 int pixel = bitmap.getPixel((int)x,(int)y);
                 int redValue = Color.red(pixel);
                 int blueValue = Color.blue(pixel);
                 int greenValue = Color.green(pixel);
                 switch (pixel){
                     case Color.RED:
-                        Log.i("Color is","RED");
+                        Log.e("Color is","RED");
                         break;
                     case Color.BLUE:
-                        Log.i("Color is","BLUE");
+                        Log.e("Color is","BLUE");
                         break;
                     case Color.GREEN:
-                        Log.i("Color is","GREEN");
+                        Log.e("Color is","GREEN");
                         break;
                     case Color.WHITE:
-                        Log.i("Color is","WHITE");
+                        Log.e("Color is","WHITE");
                         break;
                     case Color.YELLOW:
-                        Log.i("Color is","YELLOW");
+                        Log.e("Color is","YELLOW");
                         break;
                     case Color.BLACK:
-                        Log.i("Color is","BLACK");
+                        Log.e("Color is","BLACK");
                         break;
                     case Color.CYAN:
-                        Log.i("Color is","CYAN");
+                        Log.e("Color is","CYAN");
                         break;
                     case Color.MAGENTA:
-                        Log.i("Color is","MAGENTA");
+                        Log.e("Color is","MAGENTA");
                         break;
                 }
                 return true;
             }
         });
+    }
+
+    public Bitmap loadBitmapFromView(View v) {
+        Bitmap b = Bitmap.createBitmap( v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+        v.draw(c);
+        return b;
     }
 }
